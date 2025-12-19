324bf75434ee1fa2793cc70300af375d7d3b0fd1
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-19 14:27:46 +0500

    Delete Day 1 directory

--- Day 1/task/task.py
+++
@@ -1,6 +0,0 @@
-#Band Name Generator
-#This program asks user their city and pet name , based on that , it suggests band name.
-print("Welcome to the Band Name Generator🎸")
-city = input("What is the name of the city where you grew up?\n")
-pet = input("What is the name of your pet?\n")
-print("Your band name could be: " + city + " " +pet)
